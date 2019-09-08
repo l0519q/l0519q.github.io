51daf1fd793a5a274812f6a7dc93e004de879d55
l0519q <50261462+l0519q@users.noreply.github.com>
2019-09-08 19:02:09 +0800

    2019-9-8

---
+++ vuegram源码/src/components/PhoneBody.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="phone-body">
+        <div class="feed" v-if="step === 1" v-dragscroll>
+            <div class="vuegram-post">
+                <VuegramPost
+                    v-for="(post, index) in posts"
+                    :key="index"
+                    :post="post">                    
+                </VuegramPost>
+            </div>
+        </div>
+        <div v-if="step === 2">
+            <div 
+                class="selected-image"
+                :class="selectFilter"
+                :style="{backgroundImage: 'url(' + image + ')'}">
+            </div>
+            <div class="filter-container" v-dragscroll>
+                <FilterType 
+                    v-for="filter in filters" 
+                    :key="filter.name"
+                    :filter="filter"
+                    :image="image">
+                </FilterType>
+            </div>
+        </div>
+        <div v-if="step === 3">
+            <div 
+                class="selected-image"
+                :class="selectFilter"
+                :style="{backgroundImage: 'url(' + image + ')'}">
+            </div>
+            <div class="caption-container">
+                <textarea 
+                    placeholder="Write a caption..."
+                    :value="value"
+                    @input="inputText">
+                </textarea>
+            </div>           
+        </div>
+    </div>
+</template>
+<script>
+import VuegramPost from './VuegramPost'
+import FilterType from './FilterType'
+export default {
+    props: {
+        posts: Array,
+        filters: Array,
+        step: Number,
+        image: String,
+        selectFilter: String,
+        value: String
+    },
+    components: {
+        VuegramPost,
+        FilterType
+    },
+    methods: {
+        inputText(event) {
+            this.$emit("input", event.target.value)
+        }
+    }
+}
+</script>
+<style lang="scss" src="../styles/phone-body.scss">
+
+</style>
